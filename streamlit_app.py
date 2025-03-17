0170935201f1b9efd55bacc1e37831c2f6bbe59c
Taylor Isom <taylordisom@gmail.com>
2025-03-17 06:33:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -186,12 +186,12 @@ wtf is going on here?
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+    # bytes_data = uploaded_file.getvalue()
+    # st.write(bytes_data)
 
     # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+    # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    # st.write(stringio)
 
     # To read file as string:
     # string_data = stringio.read()
